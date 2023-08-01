e34125cbd9fa5c3330ca25ed837f0f8c5315f39a
squirt11e <coloradobites@gmail.com>
2023-08-01 12:20:56 -0400

    add shopify js buy and tshy buy button

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,3 +1,6 @@
+import React, { useEffect, useState } from 'react';
+import Client from 'shopify-buy';
+
 import Product from './Product';
 
 type ProductType = {
@@ -8,27 +11,43 @@ type ProductType = {
   image: string;
 };
 
-const productsData: ProductType[] = [
-  {
-    id: 1,
-    name: 'Tuschay Studios Coloring Book',
-    description: 'Description of Product 1',
-    price: 1999,
-    image: '/product1.jpg',
-  },
-  {
-    id: 2,
-    name: 'Funguys Kingdom T-Shirt',
-    description: 'Description of Product 2',
-    price: 999,
-    image: '/product2.jpeg',
-  },
-];
+// Initializing a client to return content in the store's primary language
+const client = Client.buildClient({
+  domain: 'your-shop-name.myshopify.com',
+  storefrontAccessToken: 'your-storefront-access-token',
+  apiVersion: 'v1',
+});
 
 const Products: React.FC = () => {
+  const [shopifyProducts, setShopifyProducts] = useState<ProductType[]>([]);
+  const [productError, setProductError] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Fetch all products in your shop
+    client.product.fetchAll().then(
+      (products: any) => {
+        // Set the fetched products in the component state
+        setShopifyProducts(products);
+      },
+      (error) => {
+        // Handle the fetch error and set the error state
+        setProductError('Failed to fetch products. Please try again later.');
+        console.error('Error fetching products:', error);
+      }
+    );
+  }, []);
+
+  if (productError || !shopifyProducts.length) {
+    return (
+      <div className="text-center">
+        <p>Merch not available at this time</p>
+      </div>
+    );
+  }
+
   return (
     <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-      {productsData.map((product) => (
+      {shopifyProducts.map((product) => (
         <Product key={product.id} product={product} />
       ))}
     </div>
